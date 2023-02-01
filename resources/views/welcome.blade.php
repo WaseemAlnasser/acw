f94c9ed3a0c90bfa9a937bd28d2f2fcd1c2e4040
Waseem <alnasserwaseem10@gmail.com>
2023-02-01 12:14:16 +0400

    first

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,323 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+
+    <!-- Page Title -->
+    <title>ARAB CREATIVE WOMEN</title>
+    <!-- Meta Tags -->
+    <meta name="description" content="We honor and celebrate the talent of Arab Creative Women to shine in all aspects of public life    ">
+    <meta name="keywords" content="ACW, coming soon, under construction, magazine,">
+    <meta name="author" content="ACW">
+
+    <!-- Viewport Meta-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Template Favicon & Icons Start -->
+    <link rel="icon" href="{{asset('theme/img/favicon/favicon.ico')}}" sizes="any">
+    <link rel="icon" href="{{asset('theme/img/favicon/icon.svg')}}" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="{{asset('theme/img/favicon/apple-touch-icon.png')}}">
+    <link rel="manifest" href="{{asset('theme/img/favicon/manifest.webmanifest')}}">
+    <!-- Template Favicon & Icons End -->
+
+    <!-- Facebook Metadata Start -->
+    <meta property="og:image:height" content="1200">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:title" content="Arab creative woman">
+    <meta property="og:description" content="We honor and celebrate the talent of Arab Creative Women to shine in all aspects of public life    ">
+
+    <!-- Template Styles Start -->
+    <link rel="stylesheet" type="text/css" href="{{asset('theme/css/loaders/loader.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('theme/css/plugins.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('theme/css/main.css')}}">
+    <!-- Template Styles End -->
+
+    <!-- Custom Browser Color Start -->
+    <meta name="theme-color" content="#0e0d26">
+    <meta name="msapplication-navbutton-color" content="#0e0d26">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#0e0d26">
+    <!-- Custom Browser Color End -->
+</head>
+
+<body>
+
+<!-- Loader Start -->
+<div class="loader">
+    <div class="loader__container">
+        <lottie-player src="{{asset('theme/lottie/acw1.json')}}"   speed="1"   loop  autoplay></lottie-player>
+    </div>
+</div>
+<!-- Loader End -->
+
+<!-- Menu Start -->
+<nav id="menu" class="menu">
+    <div class="menu__content">
+        <div class="container-fluid p-0 fullheight">
+            <div class="row g-0 flex-xl-row-reverse fullheight">
+                <!--Menu Data Block Start -->
+                <div class="col-12 col-xl-8 menu__data">
+                    <div class="menudata__copyright">
+                        <div class="container-fluid p-0">
+                            <div class="row g-0">
+                                <div class="col-12">
+                                    <p class="copyright">By <a href="https://acw.ae" target="_blank">ACW</a>, 2023</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--Menu Data Block End -->
+
+                <!--Menu Media Block Start -->
+                <div class="col-12 col-xl-4 menu__media">
+                    <div class="media__content">
+                        <div class="menu__image menu-image-1"></div>
+                    </div>
+                </div>
+                <!--Menu Media Block End -->
+
+            </div>
+        </div>
+    </div>
+</nav>
+<!-- Menu End -->
+
+<!-- Header Start -->
+<header id="header" class="header">
+
+    <!-- Logo Start -->
+    <div class="header__logo" style=" ">
+        <img src="{{asset('theme/img/logo.svg')}}" alt="ACW - Coming Soon" width="300" >
+    </div>
+    <!-- Logo End -->
+
+    <!-- Header Content Start -->
+    <div class="header__content fullheight-mobile d-flex align-items-center">
+        <div class="container-fluid px-0">
+            <div class="row gx-0 justify-content-end justify-content-xl-start" style="height: 70px;align-items: end;">
+                <div class="col-auto col-xl-8">
+
+                    <!-- Header Controls Block Start -->
+                    <div class="header__buttons d-flex justify-content-end align-items-center">
+
+                        <!-- Socials Desktop Start -->
+                        <div class="socials-desktop">
+                            <ul>
+                                <li>
+                                    <a href="https://www.facebook.com/profile.php?id=100089797245604&mibextid=LQQJ4d" target="_blank">
+                                        <i class="fa-brands fa-facebook"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.instagram.com/acw.ae/?igshid=YmMyMTA2M2Y%3D" target="_blank">
+                                        <i class="fa-brands fa-instagram"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.pinterest.com/acw_ae/" target="_blank">
+                                        <i class="fa-brands fa-pinterest"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://www.tiktok.com/@acw.ae" target="_blank">
+                                        <i class="fa-brands fa-tiktok"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- Socials Desktop End -->
+
+                        <!-- Notify Trigger Start -->
+                        <a id="notify-trigger" class="btn-round-mobile btn-notify btn-light-outline notify-trigger hover-white hover-white-s" href="#0">
+                            <em></em>
+                            <i class="fa-solid fa-bell"></i>
+                            <span class="btn-caption">Notify Me</span>
+                        </a>
+                        <!-- Notify Trigger End -->
+                    </div>
+                    <!-- Header Controls Block End -->
+
+                </div>
+                <div class="col-auto col-xl-4">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Header Content End -->
+
+</header>
+<!-- Header End -->
+
+<!-- Main Section Start -->
+<section id="main" class="main active">
+    <div class="main__content">
+        <div class="container-fluid p-0 fullheight">
+            <div class="row g-0 fullheight">
+
+                <!-- Main Section Intro Start -->
+                <div class="col-12 col-xl-8 main__intro">
+
+                    <!-- Intro Content Start -->
+                    <div class="intro__content">
+
+                        <!-- Headline Start -->
+                        <div id="headline" class="headline">
+                            <span class="headline__subtitle">Let’s meet</span>
+                            <h1 style="color: #5c548c">Stay tuned for something awesome.</h1>
+                        </div>
+                        <!-- Headline End -->
+
+                    </div>
+
+                </div>
+                <!-- Main Section Intro End -->
+
+                <!-- Main Section Media Start -->
+                <div class="col-12  col-xl-4 main__media">
+                    <!-- Main Section Image Background Start -->
+                    <div class="media__image media-image-2" style="">
+                        <img src="{{asset('theme/img/web4.jpg')}}">
+                    </div>
+                </div>
+                <div class="col-12 socials-mobile">
+                    <ul>
+                        <li>
+                            <a href="https://www.facebook.com/profile.php?id=100089797245604&mibextid=LQQJ4d" target="_blank">
+                                <i class="fa-brands fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/acw.ae/?igshid=YmMyMTA2M2Y%3D" target="_blank">
+                                <i class="fa-brands fa-instagram"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.pinterest.com/acw_ae/" target="_blank">
+                                <i class="fa-brands fa-pinterest"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://www.tiktok.com/@acw.ae" target="_blank">
+                                <i class="fa-brands fa-tiktok"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- Sosials Mobile End -->
+
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Main Section End -->
+<!-- Notify Popup Start -->
+<div id="notify" class="popup notify">
+    <div class="popup__container">
+
+        <!-- Popup Controls Start -->
+        <div class="popup__controls d-flex justify-content-end">
+            <a href="#0" id="notify-close" class="popup-close hover-gradient hover-gradient-s">
+                <em></em>
+                <span></span>
+            </a>
+        </div>
+        <!-- Popup Controls End -->
+
+        <!-- Popup Content Start -->
+        <div class="popup__content block-rounded-l">
+
+            <!-- Notify Popup Title Start -->
+            <div class="popup-title d-flex justify-content-center flex-column align-items-center">
+                <p class="popup-title__title">Get to know about our launch</p>
+                <p class="popup-title__text ">Subscribe to our newsletter and we will send you a notification about the launch of our brand new site</p>
+            </div>
+            <!-- Notify Popup Title End -->
+
+            <!-- Notify Form Container Start -->
+            <div class="form-container">
+
+                <!-- Reply Messages Start-->
+                <div class="reply-group subscription-ok">
+                    <i class="fa-solid fa-circle-check reply-group__icon"></i>
+                    <p class="reply-group__title txt-light">Done!</p>
+                    <span class="reply-group__text">Thanks for subscribing. We will send you a notification about our launch.</span>
+                </div>
+                <div class="reply-group subscription-error">
+                    <i class="fa-solid fa-face-frown-open reply-group__icon"></i>
+                    <p class="reply-group__title">Ooops!</p>
+                    <span class="reply-group__text">Email has already been used</span>
+                </div>
+                <!-- Reply Messages End-->
+
+                <!-- Notify Contact Form Start-->
+                <form method="post" action="{{route('subscribe')}}" class="form notify-form form-light">
+                    @csrf
+                    <input class="block-rounded-s" type="email" name="email" placeholder="Your Email" required>
+                    <button class="btn btn-l btn-light hover-gradient hover-gradient-l" type="submit">
+                        <em></em>
+                        <span class="btn-caption">Submit</span>
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Notify Popup End -->
+
+<!-- Load Scripts Start -->
+<script src="{{asset('theme/js/libs.min.js')}}"></script>
+{{--<script src="{{asset('theme/js/gallery-init.js')}}"></script>--}}
+{{--<script src="{{asset('theme/js/custom.js')}}"></script>--}}
+<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+<script>
+$('.notify-form').on('submit', function(e) {
+    e.preventDefault();
+    var form = $(this);
+    var url = form.attr('action');
+    var method = form.attr('method');
+    var data = form.serialize();
+    $.ajax({
+        url: url,
+        type: method,
+        data: data,
+        success: function(response) {
+            $('.notify').find('.form').addClass('is-hidden');
+            $('.notify').find('.subscription-ok').addClass('is-visible');
+        },
+        error: function(response) {
+            $('.notify').find('.form').addClass('is-hidden');
+            $('.notify').find('.subscription-error').addClass('is-visible');
+        }
+
+    });
+});
+
+function mailchimpCallback(resp) {
+if(resp.result === 'success') {
+$('.notify').find('.form').addClass('is-hidden');
+$('.notify').find('.subscription-ok').addClass('is-visible');
+setTimeout(function() {
+// Done Functions
+$('.notify').find('.subscription-ok').removeClass('is-visible');
+$('.notify').find('.form').delay(300).removeClass('is-hidden');
+$('.notify-form').trigger("reset");
+}, 5000);
+} else if(resp.result === 'error') {
+$('.notify').find('.form').addClass('is-hidden');
+$('.notify').find('.subscription-error').addClass('is-visible');
+setTimeout(function() {
+// Done Functions
+$('.notify').find('.subscription-error').removeClass('is-visible');
+$('.notify').find('.form').delay(300).removeClass('is-hidden');
+$('.notify-form').trigger("reset");
+}, 5000);
+}
+};
+</script>
+<!-- Load Scripts End -->
+
+</body>
+
+</html>
